0abb1f277abb566336d5d7172a56d4304d4ec552
Maamoun <maamoun.haj.najeeb@gmail.com>
2023-10-08 14:57:49 +0300

    before figuring out about update

--- multiple_images/urls.py
+++ multiple_images/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("create_image/", views.CreateImage.as_view(), name="add_image"),
     path("images/", views.ListImages.as_view(), name="list_images"),
     path("images/<int:pk>/", views.SpecificImage.as_view(), name="specific_image"),
+    path("images/update/<int:pk>/", views.UpdateImage.as_view(), name="update_image"),
 ]
--- multiple_images/views.py
+++ multiple_images/views.py
@@ -4,17 +4,20 @@ from rest_framework import response, status
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpRequest
 
+from uuid import uuid4
+
 from . import serializers, models, mixins
 
 
-class CreateImage(generics.CreateAPIView):
+class CreateImage(generics.CreateAPIView, mixins.DeleteFilesMixin):
     
     permission_classes = (permissions.AllowAny, )
     serializer_class = serializers.CreateMultipleImages
     queryset = models.MultipleImage.objects
     
     def create(self, request: HttpRequest, *args, **kwargs):
-        images_names = self.manage_uploaded_images(request)
+        images_names = self.manage_uploaded_images()
+        # images_names = self.manage_uploaded_images(request)
         
         new_request_data = request.data.copy()
         new_request_data["images"] = images_names
@@ -29,28 +32,31 @@ class CreateImage(generics.CreateAPIView):
             , status=status.HTTP_201_CREATED
             , headers=self.get_success_headers(serializer.data))
 
-    def manage_uploaded_images(self, request):
-        uploaded_images = request.FILES.getlist("images")
-        images_names = ""
-        fs = FileSystemStorage()
-        for image in uploaded_images:
-            files_names = fs.listdir(fs.location)[1]
-            unique_name = self.check_redundent(image.name, files_names)
-            images_names += f" {fs.base_url}{unique_name}"
-            fs.save(unique_name, image)
-            
-        return images_names[1:]
-    
-    def check_redundent(self, image_name, files_names, counter=-1):
-        new_name, new_extension = image_name.split(".")
-        for image in files_names:
-            old_name, old_extenstion = image.split(".")
-            if old_extenstion == new_extension and old_name == new_name:
-                counter += 1
-                image_name = f"{new_name}{counter}.{new_extension}"
-                return self.check_redundent(image_name, files_names, counter)
+    # def manage_uploaded_images(self, request: HttpRequest):
+    #     fs = FileSystemStorage()
+    #     images_names = ""
         
-        return image_name
+    #     uploaded_images = self.uploaded_images(request)
+    #     for image_obj in uploaded_images:
+    #         files_names = self.get_files_names(fs)
+    #         unique_name = self.check_redundent(image_obj.name, files_names)
+    #         images_names += f" {fs.base_url}{unique_name}"
+    #         fs.save(unique_name, image_obj)
+            
+    #     return images_names[1:]
+    
+    # def check_redundent(self, image_name, files_names):
+    #     new_name, new_extension = uuid4().hex, image_name.split(".")[1]
+    #     new_image_name = f"{new_name}.{new_extension}"
+    #     if new_image_name in files_names:
+    #         return self.check_redundent(image_name, files_names)
+    #     return new_image_name
+    
+    # def get_files_names(self, fs: FileSystemStorage):
+    #     return fs.listdir(fs.location)[1]
+    
+    # def uploaded_images(self, request: HttpRequest):
+    #     return request.FILES.getlist("images")
 
 
 class ListImages(generics.ListAPIView):
@@ -65,6 +71,19 @@ class ListImages(generics.ListAPIView):
         return response.Response({
             "data": serializer.data,
         }, status=status.HTTP_200_OK)
+
+
+class UpdateImage(generics.UpdateAPIView, mixins.DeleteFilesMixin):
+    queryset = models.MultipleImage.objects
+    serializer_class = serializers.GetImages
+    permission_classes = ()
+    
+    def get_object(self):
+        obj = super().get_object()
+        if self.request.method != "GET":
+            self.delete_files(obj)
+            self.manage_uploaded_images()
+        return obj
 
 
 class SpecificImage(generics.RetrieveDestroyAPIView, mixins.DeleteFilesMixin):
